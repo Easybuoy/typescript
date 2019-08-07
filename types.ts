8ace0721e65081d000caa50cddf5b90986d37c8f
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-07 11:46:05 +0100

    variable types

--- types.ts
+++ types.ts
@@ -2,14 +2,20 @@ let myString: string;
 let myNum: number;
 let myBool: boolean;
 let myVal: any;
-
 let objArray: Array<object>
+let myVoid: void;
+let myNull: null;
+let myUndefined: undefined;
 
 myBool = true;
 myString = "Hello World";
 myNum = 22;
 myVal = [5];
 objArray = [{me: 'a'}];
+myVoid = null;
+myNull = null;
+myUndefined = undefined;
+
 
 console.log(myVal);
 console.log(myBool);
