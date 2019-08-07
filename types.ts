082689c66e09dfbedeea884262a35456cf54a018
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-07 12:00:27 +0100

    functions

--- types.ts
+++ types.ts
@@ -2,7 +2,7 @@ let myString: string;
 let myNum: number;
 let myBool: boolean;
 let myVal: any;
-let objArray: Array<object>
+let objArray: Array<object>;
 let myVoid: void;
 let myNull: null;
 let myUndefined: undefined;
@@ -11,11 +11,10 @@ myBool = true;
 myString = "Hello World";
 myNum = 22;
 myVal = [5];
-objArray = [{me: 'a'}];
+objArray = [{ me: "a" }];
 myVoid = null;
 myNull = null;
 myUndefined = undefined;
-
 
 console.log(myVal);
 console.log(myBool);
