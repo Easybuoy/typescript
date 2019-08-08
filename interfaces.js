2cb06dbcbedb8e314caf97a1a2d2a85c1d208a49
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-08 13:02:15 +0100

    classes

--- interfaces.js
+++
@@ -1,7 +0,0 @@
-// function showTodo(todo: { title: string; text: string }) {
-//     console.log(todo)
-// }
-function showTodo(todo) {
-    console.log(todo);
-}
-showTodo({ title: "Yo", text: "me" });
